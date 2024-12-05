02e97d033ff3b51c672fba609b5feb03b8cf19dd
Ricardo <ricardo_slb_brites@live.ca>
2024-12-04 19:15:59 -0500

    Fixed migrations

--- transaction_system/settings.py
+++ transaction_system/settings.py
@@ -38,7 +38,7 @@ if not IS_HEROKU_APP:
     DEBUG = True
 
 if IS_HEROKU_APP:
-    ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]", "0.0.0.0"]
+    ALLOWED_HOSTS = ["*"]
 else:
     ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]", "0.0.0.0"]
 
